8464a5e8401e63586d55d9ffd23281a69c6ef82a
Go.Doc <97858843+OlegDoc@users.noreply.github.com>
2023-02-18 15:57:56 +0200

    add css for Home Page

--- src/modules/TrendMovie/TrendMoviesList/TrendMoviesList.jsx
+++ src/modules/TrendMovie/TrendMoviesList/TrendMoviesList.jsx
@@ -1,13 +1,15 @@
 import { Link } from 'react-router-dom';
 
+import css from './TrendMoviesList.module.css';
+
 const TrendMoviesList = ({ items }) => {
   const elements = items.map(({ id, title }) => (
-    <Link key={id} to={`/movies/${id}`}>
+    <Link className={css.trend_link} key={id} to={`/movies/${id}`}>
       <li>{title}</li>
     </Link>
   ));
 
-  return <ul>{elements}</ul>;
+  return <ul className={css.trend_list}>{elements}</ul>;
 };
 
 export default TrendMoviesList;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,14 @@
 import TrendMovies from 'modules/TrendMovie/TrendMovies';
 
+import css from './HomePage.module.css';
+
 const HomePage = () => {
-  return <TrendMovies />;
+  return (
+    <>
+      <h2 className={css.title_page}>Trending Today</h2>
+      <TrendMovies />
+    </>
+  );
 };
 
 export default HomePage;
