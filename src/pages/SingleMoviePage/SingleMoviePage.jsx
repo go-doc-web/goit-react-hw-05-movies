136a6a0efc62ac725bae6fe9b1710d1a3cd83e06
Go.Doc <97858843+OlegDoc@users.noreply.github.com>
2023-02-19 02:35:13 +0200

    create button go back

--- src/pages/SingleMoviePage/SingleMoviePage.jsx
+++ src/pages/SingleMoviePage/SingleMoviePage.jsx
@@ -1,7 +1,21 @@
+import { useNavigate } from 'react-router-dom';
+
 import MovieDetails from '../../modules/MovieDetails/MovieDetails';
 
+import css from './SingleMoviePage.module.css';
+
 const SingleMoviePage = () => {
-  return <MovieDetails />;
+  const navigate = useNavigate();
+  const goBack = () => navigate(-1);
+
+  return (
+    <>
+      <button onClick={goBack} className={css.btn}>
+        Go Back
+      </button>
+      <MovieDetails />
+    </>
+  );
 };
 
 export default SingleMoviePage;
